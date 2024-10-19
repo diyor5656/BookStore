6f6079bda8e9882a401d9ea3375dedb3bfb7300a
diyor5656 <absalomovdiyorbek0@gmail.com>
2024-10-19 17:38:03 +0500

    Добавьте файлы проекта.

---
+++ BookStore/Context/BookStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Numerics;
+
+public class BookStoreContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            @"Server=localhost;Database=BookStore;User Id=postgres;Password=19871984;");
+    }
+}
---
+++ BookStore/Controllers/BookControllers.cs
@@ -0,0 +1,44 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookStoreContext _context;
+
+        public BookController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet("{Id}")]
+        public Book GetBook(int Id)
+        {
+            var book = _context.Books.FirstOrDefault(book => book.Id == Id);
+            return book; 
+        }
+
+       
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var books = _context.Books.ToList();
+            return books;
+        }
+
+        
+        [HttpPost]
+        public int PostBook(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges(); 
+            return book.Id;
+        }
+    }
+}
